041078ecbbfe789bac8ab16cadcd223e25ae77ad
devonxmurray <murraydevon14@gmail.com>
2022-04-15 11:25:13 -0400

    eight commit

--- public/script.js
+++ public/script.js
@@ -5,6 +5,9 @@ document.addEventListener('DOMContentLoaded', function() {
   M.Dropdown.init(elems, {coverTrigger:false});
 });
 
+
+
+
 function onSignIn(googleUser) {
   var profile = googleUser.getBasicProfile();
   console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
